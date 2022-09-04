7f725bfbe0c958767882ad8842d3d88c65f72b7e
Aaditya Dev Sharma <90966493+Aadi1659@users.noreply.github.com>
2022-09-04 18:46:43 +0530

    V 0.0.1 Date 4/09/2022
    Added all the currently existing files in intro to node folder

---
+++ nodeJS/index.js
@@ -0,0 +1,9 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+
+var mySuperHero = superheroes.random();
+console.log(mySuperHero);
+
+var mySuperVillain = supervillains.random();
+console.log(mySuperVillain);
